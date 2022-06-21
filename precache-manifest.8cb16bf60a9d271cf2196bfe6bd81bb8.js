37dadad8b20fbd26d0aa3f5c9a5bf8f38df8f776
Jayesh Mulwani <jayesh.mulwani93@gmail.com>
2022-06-21 19:35:42 +0530

    Updates

---
+++ precache-manifest.8cb16bf60a9d271cf2196bfe6bd81bb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32b45386b871282a9359",
+    "url": "/MyReads/static/css/main.439b6659.chunk.css"
+  },
+  {
+    "revision": "32b45386b871282a9359",
+    "url": "/MyReads/static/js/main.32b45386.chunk.js"
+  },
+  {
+    "revision": "8a7635b136b29afa6533",
+    "url": "/MyReads/static/js/1.8a7635b1.chunk.js"
+  },
+  {
+    "revision": "970c0060ff174c587b9c",
+    "url": "/MyReads/static/js/runtime~main.970c0060.js"
+  },
+  {
+    "revision": "af6270954afdb643b98d6ec08c6ab18a",
+    "url": "/MyReads/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MyReads/precache-manifest.0ac540aa4701b9b92e970d0a439c3113.js"
+  "/MyReads/precache-manifest.8cb16bf60a9d271cf2196bfe6bd81bb8.js"
 );
 
 workbox.clientsClaim();
